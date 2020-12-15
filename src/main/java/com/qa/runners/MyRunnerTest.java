c15295169c67d6f68ce759253281f89b1972d431
surajit sarkar <surajit.sarkar@globant.com>
2020-12-15 08:17:29 +0530

    initial commit

---
+++ src/main/java/com/qa/runners/MyRunnerTest.java
@@ -0,0 +1,18 @@
+package com.qa.runners;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber","summary"}
+        , snippets = CAMELCASE
+        , dryRun = true
+        , monochrome = true
+       // , tags = {"@foo", "not @bar"}
+)
+public class MyRunnerTest {
+}
